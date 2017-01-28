f524fdc61e43a9921eef902719c74abef139bdb8
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-01-28 05:52:38 -0800

    Use data from jsonp

--- js/darksky.js
+++ js/darksky.js
@@ -1,10 +1,47 @@
-function logResults(json){
-  var data = json;
-  document.getElementById('api-data').innerHTML = data;
+// Create the function the JSON data will be passed to.
+function myfunc(json) {
+  // test container
+  var test_target = document.getElementById('api-data');
+  // current date (timestamp)
+  var unixDate = $.now();
+
+  // converts timestamp array
+  function scDateTime(unixtimestamp){
+    // convert timestamp to date...
+    var rawDate = new Date(unixtimestamp);
+    // date to string...
+    var strDate = rawDate.toString();
+    // string to array
+    var arrDate = strDate.split(' ');
+    // return array
+    return arrDate;
+  }
+  // returns mmm dd, yyyy
+  function scDate (a){
+    var date = a[1] +" "+ a[2] +", "+ a[3];
+    return date;
+  }
+  // returns hh:mm:ss
+  function scTime (a){
+    var time = a[4];
+    return time;
+  }
+  // current date and time
+  now = scDate(scDateTime(unixDate))+"<br>"+scTime (scDateTime(unixDate));
+
+  test_target.innerHTML = now;
 }
 
+
+
 $.ajax({
-  url: "https://api.darksky.net/forecast/api/lat,long",
-  dataType: "jsonp",
-  jsonpCallback: "logResults"
+  type: "GET",
+  url: "https://api.darksky.net/forecast/",
+  dataType: 'jsonp',
+  jsonpCallback: 'myfunc', // the function to call
+  jsonp: 'callback', // name of the var specifying the callback in the request
+  error: function (xhr, errorType, exception) {
+    var errorMessage = exception || xhr.statusText;
+    alert("Excep:: " + exception + "Status:: " + xhr.statusText);
+  }
 });
